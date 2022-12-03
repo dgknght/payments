348496bbd9b0c927e89fcf83ac6f6bdcd0e8a653
dgknght <dougbknight@gmail.com>
2022-12-03 10:42:20 -0600

    raw template

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dgknght.payments "0.1.0-SNAPSHOT"
+  :description "Library for interacting with various payment providers"
+  :url "http://github.com/dgknght/payments"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
---
+++ test/dgknght/payments_test.clj
@@ -0,0 +1,7 @@
+(ns dgknght.payments-test
+  (:require [clojure.test :refer :all]
+            [dgknght.payments :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
