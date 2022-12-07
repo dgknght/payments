1c31b8240c9fab981fe1d4a30f42eb4259fb2bbf
dgknght <dougbknight@gmail.com>
2022-12-07 14:47:11 -0600

    assert required arguments

--- src/dgknght/payments/paypal.cljs
+++ src/dgknght/payments/paypal.cljs
@@ -7,7 +7,11 @@
   (pp/buttons elem-id
     {:create-order fn-that-initializes-payment
      :on-approve fn-that-finalizes-payment})"
-  [{:keys [element-id _create-order _on-approve]}]
+  [{:keys [element-id _create-order _on-approve] :as args}]
+  {:pre [(:element-id args)
+         (:create-order args)
+         (:on-approve args)]}
+
   (let [paypal (.-paypal js/window)
         result (.Buttons paypal (js-obj "one" 1))]
     (.render result element-id)))
--- test/dgknght/payments/paypal_test.cljs
+++ test/dgknght/payments/paypal_test.cljs
@@ -1,19 +1,33 @@
 (ns dgknght.payments.paypal-test
-  (:require [cljs.test :refer [deftest is]]
+  (:require [cljs.test :refer [deftest testing is]]
             [dgknght.payments.paypal.mocks :refer-macros [with-paypal-mocks]]
             [dgknght.payments.paypal :as pp]))
 
 (deftest initialize-the-buttons
-  (with-paypal-mocks [calls]
-    (is (not
-          (nil?
-            (pp/buttons {:element-id "#button-container"
-                         :create-order :add-channel-or-callback-fn
-                         :on-approve :add-channel-or-callback-fn})))
-        "The PayPal Buttons instance is returned")
-    (let [[c :as cs] (get-in @calls [:render] [])]
-      (is (= 1 (count cs))
-          "The render method is called once")
-      (is (= ["#button-container"]
-             c)
-          "The render method is called with the ID of the element where the buttons are to be rendered"))))
+  (testing "with valid arguments"
+    (with-paypal-mocks [calls]
+      (is (not
+            (nil?
+              (pp/buttons {:element-id "#button-container"
+                           :create-order :add-channel-or-callback-fn
+                           :on-approve :add-channel-or-callback-fn})))
+          "The PayPal Buttons instance is returned")
+      (let [[c :as cs] (get-in @calls [:render] [])]
+        (is (= 1 (count cs))
+            "The render method is called once")
+        (is (= ["#button-container"]
+               c)
+            "The render method is called with the ID of the element where the buttons are to be rendered"))))
+  (testing "with invalid arguments"
+    (is (thrown-with-msg? js/Error #"Assert failed: \(:element-id args\)"
+                 (pp/buttons {:create-order :add-channel
+                              :on-approve :add-channel}))
+        "element-id is required")
+    (is (thrown-with-msg? js/Error #"Assert failed: \(:create-order args\)"
+                 (pp/buttons {:element-id "#button-container"
+                              :on-approve :add-channel}))
+        "create-order is required")
+    (is (thrown-with-msg? js/Error #"Assert failed: \(:on-approve args\)"
+                 (pp/buttons {:element-id "#button-container"
+                              :create-order :add-channel}))
+        "on-approve is required")))
