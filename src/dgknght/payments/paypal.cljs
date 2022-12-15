1b9295b69c38f7f24d4ee89297ddd7dd1598bb03
dgknght <dougbknight@gmail.com>
2022-12-15 15:30:53 -0600

    handle onCancel

--- src/dgknght/payments/paypal.cljs
+++ src/dgknght/payments/paypal.cljs
@@ -28,6 +28,22 @@
         (a/go
           (a/>! c args))))))
 
+(defn- buttons-config
+  [{:keys [create-order on-approve on-cancel]}]
+  (clj->js
+    (cond-> {"createOrder" (fn [data actions]
+                             (->promise create-order
+                                        {:data (js->clj data)
+                                         :actions (js->clj actions)}))
+             "onApprove" (fn [data actions]
+                           (->promise on-approve
+                                      {:data (js->clj data)
+                                       :actions (js->clj actions)}))}
+      on-cancel (assoc
+                  "onCancel" (fn [data]
+                               (->promise on-cancel
+                                          {:data (js->clj data)}))))))
+
 (defn buttons
   "Create and return an instance that manages
   the PayPal buttons.
@@ -35,23 +51,13 @@
   (pp/buttons elem-id
     {:create-order fn-that-initializes-payment
      :on-approve fn-that-finalizes-payment})"
-  [{:keys [element-id create-order on-approve] :as args}]
+  [{:keys [element-id] :as args}]
   {:pre [(:element-id args)
          (:create-order args)
          (:on-approve args)]}
 
   (if-let [paypal (.-paypal js/window)]
-    (let [btns (.Buttons paypal
-                         (js-obj "createOrder"
-                                 (fn [data actions]
-                                   (->promise create-order
-                                              {:data (js->clj data)
-                                               :actions (js->clj actions)}))
-                                 "onApprove"
-                                 (fn [data actions]
-                                   (->promise on-approve
-                                              {:data (js->clj data)
-                                               :actions (js->clj actions)}))))]
+    (let [btns (.Buttons paypal (buttons-config args))]
       (.render btns element-id)
       btns)
     (.error js/console "Unable to load the PayPal library.")))
